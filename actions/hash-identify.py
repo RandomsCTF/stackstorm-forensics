4a33da18a5af913a925177c2029f8dd8b52c36ae
Edward Medvedev <edward.medvedev@gmail.com>
2015-09-15 16:59:09 +0600

    Identifier —> identify

--- actions/hash-identify.py
+++ actions/hash-identify.py
@@ -4,10 +4,10 @@ from lib.hashtag import identify_hash
 from st2actions.runners.pythonrunner import Action
 
 __all__ = [
-    'HashIdentifierAction',
+    'HashIdentifyAction',
 ]
 
 
-class HashIdentifierAction(Action):
+class HashIdentifyAction(Action):
     def run(self, hash):
         return ", ".join(identify_hash(hash)[:3])
